83724f81a34fa8ac15f0bb443ae70782fb685e00
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2025-10-06 14:41:45 -0400

    Add bash commands file

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -7,3 +7,5 @@ alias wd='${CMD_UTIL_HOME}/bash/wd.sh'
 alias cwd='cd $(${CMD_UTIL_HOME}/bash/wd.sh echo)'
 alias pushwd='pushd $(${CMD_UTIL_HOME}/bash/wd.sh echo)'
 alias bjob='${CMD_UTIL_HOME}/bash/bjob.sh'
+alias help='less ${CMD_UTIL_HOME}/bash/bash.txt'
+
