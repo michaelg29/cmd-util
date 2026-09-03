139114d5bb63d4a613d7b8c972f4269804fa1442
Michael Grieco <30534878+michaelg29@users.noreply.github.com>
2026-09-03 14:47:47 -0400

    Organize aliases and tack venv onto cwd alias

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -7,8 +7,15 @@ alias pinfo='ps -Flww -p'
 # cmd-util scripts
 CMD_UTIL_HOME="${HOME}/bin/cmd-util"
 alias wd='${CMD_UTIL_HOME}/bash/wd.sh'
-alias cwd='cd $(${CMD_UTIL_HOME}/bash/wd.sh echo)'
+#alias cwd='cd $(${CMD_UTIL_HOME}/bash/wd.sh echo)'
+alias cwd='$(command -v deactivate &> /dev/null) && deactivate; cd $(${CMD_UTIL_HOME}/bash/wd.sh echo); ( [ -f .venv/bin/activate ] && echo -n "Source .venv/bin/activate? [n|y] n: " && read ny && [ "$ny" == "y" ] ) && source .venv/bin/activate;'
 alias pushwd='pushd $(${CMD_UTIL_HOME}/bash/wd.sh echo)'
 alias bjob='${CMD_UTIL_HOME}/bash/bjob.sh'
 alias help='less ${CMD_UTIL_HOME}/bash/bash.txt'
+
+# backtracking
+alias b='cd ../'
+alias bb='cd ../../'
+alias bbb='cd ../../../'
+alias bbbb='cd ../../../../'
 
--- env.sh
+++ env.sh
@@ -8,13 +8,8 @@ echo "Sourcing cmd-util from $CMD_UTIL_HOME"
 
 # Source tools
 source ${CMD_UTIL_HOME}/bash/tools.sh
+source ${CMD_UTIL_HOME}/bash/aliases.sh
 export PATH="${PATH}:${CMD_UTIL_HOME}/bin:${CMD_UTIL_HOME}/bash"
-alias wd='${CMD_UTIL_HOME}/bash/wd.sh'
-alias cwd='cd $(${CMD_UTIL_HOME}/bash/wd.sh echo)'
-alias pushwd='pushd $(${CMD_UTIL_HOME}/bash/wd.sh echo)'
-alias bjob='${CMD_UTIL_HOME}/bash/bjob.sh'
-alias ntee='${CMD_UTIL_HOME}/bash/named_tee.sh'
-alias help='less ${CMD_UTIL_HOME}/bash/bash.txt'
 
 # Optionally source prompt modification
 [ ! -z "$CMD_UTIL_PROMPT" ] && source ${CMD_UTIL_HOME}/bash/prompt.sh
